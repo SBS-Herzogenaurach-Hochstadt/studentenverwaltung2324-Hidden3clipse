405b7b6763bb5d4ac8bc95139bdd175dba7e1a2d
Fabian Rödel <Stoepsel033@gmail.com>
2024-06-18 11:09:15 +0200

    18.06.2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
+import javax.xml.crypto.Data;
+
+import de.milchreis.uibooster.UiBooster;
+import de.milchreis.uibooster.model.Form;
+import de.sbs.fswi1.services.DataAccessObject;
+
 public class Main {
+
+
+    private static DataAccessObject dao = new DataAccessObject("/Users/student/Documents/Data/studenten.csv");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        new UiBooster().showConfirmDialog(
+                "Möchtest du einen neuen Studenten anlegen?",
+                "Bist du sicher?",
+                () -> System.out.println("Action accepted"),
+                () -> System.out.println("Action declined"));
+
+        UiBooster booster = new UiBooster();
+        Form form = booster.createForm("Neuer Student")
+            .addText("Vorname:")
+            .addText("Nachname:")
+            .addDatePicker("Geburtstag:")
+            .addSelection("In welcher Klasse sind Sie?",Arrays.asList("FSWI-1", "FSWI-2"))
+            .show();
+        
+    
     }
 }
